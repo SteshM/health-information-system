bd4cb07ee4430f9a42c68df47bab8963486d4502
SteshM <stella.mutua@imali.click>
2025-04-25 11:38:17 +0300

    implement API to create health programs

--- src/main/java/com/example/health_information_system/controllers/HealthProgramController.java
+++ src/main/java/com/example/health_information_system/controllers/HealthProgramController.java
@@ -1,6 +1,15 @@
 package com.example.health_information_system.controllers;
 
+import com.example.health_information_system.dtos.requests.HealthProgramCreateDTO;
+import com.example.health_information_system.dtos.responses.ResponseDTO;
+import com.example.health_information_system.dtos.responses.StatusCodes;
+import com.example.health_information_system.services.HealthProgramService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class HealthProgramController {
+    private final HealthProgramService healthProgramService;
+
+    @PostMapping("/create")
+    public ResponseDTO<HealthProgramCreateDTO>createHealthProgram(
+            @Valid @RequestBody HealthProgramCreateDTO healthProgramCreateDTO
+    ){
+        healthProgramService.createProgram(healthProgramCreateDTO);
+        return new ResponseDTO<>(
+                StatusCodes.SUCCESS,
+                "successfully created a health program"
+        );
+
+    }
 }
--- src/main/java/com/example/health_information_system/services/HealthProgramService.java
+++ src/main/java/com/example/health_information_system/services/HealthProgramService.java
@@ -1,11 +1,26 @@
 package com.example.health_information_system.services;
 
+import com.example.health_information_system.dtos.requests.HealthProgramCreateDTO;
+import com.example.health_information_system.models.HealthProgramEntity;
+import com.example.health_information_system.repositories.HealthProgramRepo;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class HealthProgramService {
+    ModelMapper modelMapper = new ModelMapper();
+    private final HealthProgramRepo healthProgramRepo;
+
+    public void createProgram(
+            @Valid HealthProgramCreateDTO healthProgramCreateDTO) {
+        HealthProgramEntity healthProgramEntity = modelMapper.map
+                (healthProgramCreateDTO,HealthProgramEntity.class);
+        healthProgramRepo.save(healthProgramEntity);
+
+    }
 }
