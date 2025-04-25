657a6f6a1cd9e0b380003e8bbb760e453819526d
SteshM <stella.mutua@imali.click>
2025-04-25 13:06:27 +0300

    implement API to create a client

--- src/main/java/com/example/health_information_system/controllers/ClientController.java
+++ src/main/java/com/example/health_information_system/controllers/ClientController.java
@@ -1,6 +1,13 @@
 package com.example.health_information_system.controllers;
 
+import com.example.health_information_system.dtos.requests.ClientCreateDTO;
+import com.example.health_information_system.dtos.responses.ResponseDTO;
+import com.example.health_information_system.dtos.responses.StatusCodes;
+import com.example.health_information_system.services.ClientService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class ClientController {
+    private final ClientService clientService;
+
+    @PostMapping("create")
+    public ResponseDTO<ClientCreateDTO>createClient(
+            @Valid @RequestBody()ClientCreateDTO clientCreateDTO
+    ){
+        clientService.createClient(clientCreateDTO);
+        return new ResponseDTO<>(
+                StatusCodes.SUCCESS,
+                "Successfully created a client"
+        );
+    }
 }
--- src/main/java/com/example/health_information_system/services/ClientService.java
+++ src/main/java/com/example/health_information_system/services/ClientService.java
@@ -1,9 +1,24 @@
 package com.example.health_information_system.services;
 
+import com.example.health_information_system.dtos.requests.ClientCreateDTO;
+import com.example.health_information_system.models.ClientEntity;
+import com.example.health_information_system.repositories.ClientRepo;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ClientService {
+    ModelMapper modelMapper = new ModelMapper();
+    private final ClientRepo clientRepo;
+
+    public void createClient(
+            @Valid ClientCreateDTO clientCreateDTO) {
+        ClientEntity clientEntity = modelMapper.map(
+                clientCreateDTO,ClientEntity.class);
+        clientRepo.save(clientEntity);
+
+    }
 }
